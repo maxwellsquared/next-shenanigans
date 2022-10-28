70ff69286e7603282c741b9f3130605a4d3b68e7
Max K <maxkuhn@gmail.com>
2022-10-27 17:56:31 -0700

    add SSR function

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -15,8 +15,7 @@ export default function Car({ car }) {
     </>)
 }
 
-export async function getStaticProps({ params }) {
-
+export async function getServerSideProps({ params }) {
     const req = await fetch(`http://localhost:3000/${params.id}.json`);
     const data = await req.json();
 
@@ -25,7 +24,27 @@ export async function getStaticProps({ params }) {
     }
 }
 
-export async function getStaticPaths() {
-    const req = await fetch('http://localhost:3000/cars.json')
+// export async function getStaticProps({ params }) {
 
-}
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+
+//     return {
+//         props: { car: data },
+//     }
+// }
+
+// export async function getStaticPaths() {
+//     const req = await fetch('http://localhost:3000/cars.json');
+//     const data = await req.json();
+
+//     const paths = data.map(car => {
+//         return { params: {id:car}}
+//     })
+
+//     return {
+//         paths,
+//         fallback: false
+//     };
+
+// }
